e1c73c75574e3c140fcc193acc4923b6f231efd7
chrisziegler <chrisrziegler@gmail.com>
2018-03-24 22:31:30 -0700

    handled Timer refresh from onChange input, rather than onSubmit form

--- src/components/Countdown/Datepicker.js
+++ src/components/Countdown/Datepicker.js
@@ -9,19 +9,26 @@ export default class Datepicker extends Component {
   };
 
   handleDateChange = ({ target: { value } }) => {
-    const date = moment(value, 'MM-DD-YYYY');
+    const date = moment(value),
+      valid = date.isValid() && date.isAfter(moment());
     this.setState({
+      valid,
+      // store our e.target.value in component state for controlled input
       date: value,
-      valid: date.isValid() && date.isAfter(moment()),
       dirty: true
     });
+    // if valid set nextDate on application state
+    // currentDate is calculated there from moment()
+    // duration is calculated when getRemainingTime is called from Timer in index.js
+    valid && this.props.onDateReset(date);
   };
 
-  handleDateSubmit = e => {
-    const { valid, date } = this.state;
-    e.preventDefault();
-    valid && this.props.onDateReset(moment(date));
-  };
+  // REMOVED FORM AND FORM SUBMIT THIS HANDLED
+  // handleDateSubmit = e => {
+  //   const { valid, date } = this.state;
+  //   e.preventDefault();
+  //   valid && this.props.onDateReset(moment(date));
+  // };
 
   render() {
     let { date, valid, dirty } = this.state,
@@ -30,31 +37,24 @@ export default class Datepicker extends Component {
     valid && dirty && (classes += ' is-success');
     !valid && dirty && (classes += ' is-danger');
     return (
-      <form onSubmit={this.handleDateSubmit}>
-        <div
-          className="field is-grouped is-grouped-centered"
-          style={{ marginBottom: 40 }}
-        >
-          <p className="control has-text-centered">
-            <input
-              className={classes}
-              onChange={this.handleDateChange}
-              value={date}
-              placeholder="MM/DD/YYYY"
-            />
-            {!valid && (
-              <i className="help is-danger is-size-6">
-                Please type a valid (and future) date!
-              </i>
-            )}
-          </p>
-          <p className="control">
-            <button className="button button is-white is-outlined">
-              Reset
-            </button>
-          </p>
-        </div>
-      </form>
+      <div
+        className="field is-grouped is-grouped-centered"
+        style={{ marginBottom: 40 }}
+      >
+        <p className="control has-text-centered">
+          <input
+            className={classes}
+            onChange={this.handleDateChange}
+            value={date}
+            placeholder="MM/DD/YYYY"
+          />
+          {!valid && (
+            <i className="help is-danger is-size-6">
+              Please type a valid (and future) date!
+            </i>
+          )}
+        </p>
+      </div>
     );
   }
 }
